8c5da1acf36d9357773d08afaa2dc77303ffcc26
sarank1634 <saravanakumar1634@gmail.com>
2025-08-08 14:33:16 +0530

    albium schema created

--- spotify-Backend/models/albumModel.js
+++ spotify-Backend/models/albumModel.js
@@ -1,5 +1,15 @@
+import mongoose from "mongoose";
 
 
+const albumSchema = new mongoose.Schema({
+
+    name: { type: String, require: true},
+    desc: { type: String, require: true},
+    bgColor: { type: String, require: true},
+    image: { type: String, require: true }
+})
+const albumModel = mongoose.model.album || mongoose.model("album",albumSchema);
 
 
+export default albumModel;
 
