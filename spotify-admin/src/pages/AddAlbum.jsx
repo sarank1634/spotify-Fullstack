ea00568e9f387a8b6cde2770e0aae3824a147fce
sarank1634 <saravanakumar1634@gmail.com>
2025-08-10 00:13:00 +0530

    add album song ui completed

--- spotify-admin/src/pages/AddAlbum.jsx
+++ spotify-admin/src/pages/AddAlbum.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { assets } from '../assets/assets';
 
 
 const AddAlbum = () => {
   return (
-    <div></div>
+    <form className="flex flex-col items-start gap-8 text-gray-600">
+      <div className="flex flex-col gap-4">
+          <p>upload image</p>
+          <input type="file" id="image" accept='image/*' hidden />
+          <label htmlFor="image">
+            <img className='w-24 cursor-pointer' src={assets.upload_area} alt="" />
+          </label>
+      </div>
+      <div className="flex flex-col gap-4">
+        <p>Album Name</p>
+        <input className='bg-transparent outline-greeen-600 border-2 border-gray-400 p-2.5 w-[max(max(40vw, 250px)]' type="text" placeholder='Type here' />
+      </div>
+      <div className="flex flex-col gap-4">
+        <p>Album description</p>
+        <input className='bg-transparent outline-greeen-600 border-2 border-gray-400 p-2.5 w-[max(max(40vw, 250px)]' type="text" placeholder='Type here' />
+      </div>
+      <div className="flex flex-col gap-3">
+        <p>Background Color</p>
+        <input type="color" />
+        <button className='text-base bg-black text-white py-2.5 px-14  cursor-pointer' type="submit" value="Add">Add</button>
+      </div>
+    </form>
   )
 }
  export default AddAlbum;
